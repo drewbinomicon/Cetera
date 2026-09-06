c4e66bdc8801902b6a686921d892912678de4623
Andrew Harris <andrew2325601@gmail.com>
2026-09-05 21:41:27 -0500

    Add files via upload
    This is a front end for ect written in C++ and Python3 using tk

--- cetera.cpp
+++ cetera.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2026 [Your Name or Organization]
+* Copyright (C) 2026 Andrew D. Harris <andrew2325@gnail.com>
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
